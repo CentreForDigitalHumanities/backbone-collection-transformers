83f27850f3366e1ef708aa4b4ef54408692e430c
Julian Gonggrijp <j.gonggrijp@uu.nl>
2024-04-10 00:22:12 +0200

    squash! Outfactor FilteredCollection from readit-interface (WIP)
    start converting the source to suitable javascript

--- src/filtered-collection.js
+++ src/filtered-collection.js
@@ -1,27 +1,78 @@
 import {
     extend,
-    defaultsDeep,
+    defaults,
     omit,
     iteratee,
     isFunction,
-    ListIterator,
-} from 'lodash';
-import {
-    Model as BModel,
-    Collection as BCollection,
-} from 'backbone';
+} from 'underscore';
+import { Model, Collection } from 'backbone';
+import mixin from '@uu-cdh/backbone-util';
 
-import mixin from '../core/mixin';
-import Model from '../core/model';
-import Collection from '../core/collection';
-import ProxyMixin from './collection-proxy';
+import ProxyMixin from './collection-proxy.js';
 
-type AnyFunction = (...args: any[]) => any;
-// surprising inconsistency in @types/lodash
-type IterateeParam = string | object | AnyFunction;
+var FilteredCollectionMixin = {
+    criterion: FilterCriterion<M>;
+    matches: AnyFunction;
 
-export
-type FilterCriterion<M extends BModel> = ListIterator<M, boolean>;
+    constructor(underlying, criterion, options) {
+        var comparator = underlying.comparator;
+        options = defaults({}, options, {
+            underlying,
+            criterion,
+            model: underlying.model,
+        }, comparator && { comparator } || {});
+        var initialModels = underlying.filter(criterion);
+        var instance = Reflect.construct(Base, [initialModels, options], new.target || FilteredCollection);
+        instance.listenTo(underlying, {
+            add: instance.proxyAdd,
+            remove: instance.proxyRemove,
+            reset: instance.proxyReset,
+            sort: instance.proxySort,
+            change: instance.proxyChange,
+        });
+        return instance;
+    },
+
+    preinitialize(models[], {underlying, criterion}) {
+        var matches;
+        if (isFunction(criterion)) {
+            matches = criterion;
+        } else {
+            var wrappedIterator = iteratee(criterion);
+            matches = model => wrappedIterator(model.attributes);
+        }
+        extend(this, {_underlying: underlying, criterion, matches});
+    },
+
+    /**
+     * Forwarding methods. You are not supposed to invoke these yourself.
+     */
+
+    proxyAdd(model, collection, options) {
+        if (this.matches(model)) this.add(model, omit(options, 'at'));
+    },
+
+    proxyRemove(model, collection, options) {
+        this.remove(model, options);
+    },
+
+    proxyReset(collection, options) {
+        this.reset(this._underlying.filter(this.criterion), options);
+    },
+
+    proxySort(collection, options) {
+        if (this.comparator) this.sort(options);
+    },
+
+    proxyChange(model, options) {
+        // attributes changed, so we need to re-evaluate the filter criterion.
+        if (this.matches(model)) {
+            this.add(model, options);
+        } else {
+            this.remove(model, options);
+        }
+    },
+};
 
 /**
  * Synchronized filtered read-only proxy to a Backbone.Collection.
@@ -35,83 +86,14 @@ type FilterCriterion<M extends BModel> = ListIterator<M, boolean>;
  *
  * Example of usage:
 
-    let myFilteredProxy = new FilteredCollection(theRawCollection, model => {
+    var myFilteredProxy = new FilteredCollection(theRawCollection, model => {
         return model.has('@id');
     });
     myFilteredProxy.forEach(...);
     myFilteredProxy.on('add', ...);
 
  */
-interface FilteredCollection<
-    M extends BModel = Model,
-    U extends BCollection<M> = Collection<M>
-> extends ProxyMixin<M, U> {}
-class FilteredCollection<
-    M extends BModel = Model,
-    U extends BCollection<M> = Collection<M>
-> extends Collection<M> {
-    criterion: FilterCriterion<M>;
-    matches: AnyFunction;
-
-    constructor(underlying: U, criterionArg: IterateeParam, options?: any) {
-        const criterion = criterionArg as FilterCriterion<M>;
-        const comparator = underlying.comparator;
-        options = defaultsDeep(options || {}, {
-            underlying,
-            criterion,
-            model: underlying.model,
-        }, comparator && { comparator } || {});
-        const initialModels = underlying.filter(criterion);
-        super(initialModels, options);
-        underlying.on({
-            add: this.proxyAdd,
-            remove: this.proxyRemove,
-            reset: this.proxyReset,
-            sort: this.proxySort,
-            change: this.proxyChange,
-        }, this);
-    }
-
-    preinitialize(models: M[], {underlying, criterion}): void {
-        let matches: AnyFunction;
-        if (isFunction(criterion)) {
-            matches = criterion;
-        } else {
-            const wrappedIterator = iteratee(criterion as IterateeParam);
-            matches = model => wrappedIterator(model.attributes);
-        }
-        extend(this, {_underlying: underlying, criterion, matches});
-    }
-
-    /**
-     * Forwarding methods. You are not supposed to invoke these yourself.
-     */
-
-    proxyAdd(model: M, collection: U, options: any): void {
-        if (this.matches(model)) this.add(model, omit(options, 'at'));
-    }
-
-    proxyRemove(model: M, collection: U, options: any): void {
-        this.remove(model, options);
-    }
-
-    proxyReset(collection: U, options: any): void {
-        this.reset(this._underlying.filter(this.criterion), options);
-    }
-
-    proxySort(collection: U, options: any): void {
-        if (this.comparator) this.sort(options);
-    }
-
-    proxyChange(model: M, options: any): void {
-        // attributes changed, so we need to re-evaluate the filter criterion.
-        if (this.matches(model)) {
-            this.add(model, options);
-        } else {
-            this.remove(model, options);
-        }
-    }
-}
-mixin(FilteredCollection.prototype, ProxyMixin.prototype);
+class FilteredCollection extends Collection
+mixin(FilteredCollection.prototype, ProxyMixin);
 
 export default FilteredCollection;
